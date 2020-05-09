c7103947320181086900ff7780771e30591a9036
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-05-09 00:41:24 -0600

    Ejercicio 2

---
+++ Ejemplo2-Gui9/Ejemplo1-Gui9/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Ejemplo1_Gui9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnordenar = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.txtnumeros = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnordenar);
+            this.groupBox1.Controls.Add(this.btnlimpiar);
+            this.groupBox1.Controls.Add(this.btnagregar);
+            this.groupBox1.Controls.Add(this.txtnumeros);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(174, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(631, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controles";
+            // 
+            // btnordenar
+            // 
+            this.btnordenar.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnordenar.FlatAppearance.BorderSize = 0;
+            this.btnordenar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnordenar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnordenar.ForeColor = System.Drawing.Color.White;
+            this.btnordenar.Location = new System.Drawing.Point(429, 34);
+            this.btnordenar.Name = "btnordenar";
+            this.btnordenar.Size = new System.Drawing.Size(159, 33);
+            this.btnordenar.TabIndex = 3;
+            this.btnordenar.Text = "O R D E N A R";
+            this.btnordenar.UseVisualStyleBackColor = false;
+            this.btnordenar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.Firebrick;
+            this.btnlimpiar.FlatAppearance.BorderSize = 0;
+            this.btnlimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlimpiar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.ForeColor = System.Drawing.Color.White;
+            this.btnlimpiar.Location = new System.Drawing.Point(339, 34);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(84, 35);
+            this.btnlimpiar.TabIndex = 2;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnagregar.FlatAppearance.BorderSize = 0;
+            this.btnagregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnagregar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnagregar.ForeColor = System.Drawing.Color.White;
+            this.btnagregar.Location = new System.Drawing.Point(245, 34);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(88, 35);
+            this.btnagregar.TabIndex = 1;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = false;
+            this.btnagregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtnumeros
+            // 
+            this.txtnumeros.Location = new System.Drawing.Point(45, 42);
+            this.txtnumeros.Name = "txtnumeros";
+            this.txtnumeros.Size = new System.Drawing.Size(150, 27);
+            this.txtnumeros.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 149);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(990, 508);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(230)))), ((int)(((byte)(255)))));
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(982, 478);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Heap";
+            this.tabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1014, 669);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnordenar;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.TextBox txtnumeros;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+    }
+}
+
---
+++ Ejemplo2-Gui9/Ejemplo1-Gui9/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Ejemplo1_Gui9
+{
+    public partial class Form1 : Form
+    {
+
+       
+        int xo, yo, tam; 
+        bool ec = false; 
+        bool estado = false; 
+        int n = 0,  i = 1;  
+        
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = int.Parse(txtnumeros.Text);
+
+                Array.Resize<int>(ref Arreglo_numeros, i + 1); 
+                Arreglo_numeros[i] = num; 
+                Array.Resize<Button>(ref Arreglo, i + 1);
+                Arreglo[i] = new Button();
+               
+                Arreglo[i].Text = Arreglo_numeros[i].ToString(); 
+                Arreglo[i].Height = 40; 
+                Arreglo[i].Width = 40; 
+                Arreglo[i].BackColor = Color.DarkViolet;
+                Arreglo[i].ForeColor=Color.White ;
+                Arreglo[i].Font = new Font("Century Gothic", 12);
+                Arreglo[i].Location = new Point(xo, yo) + new Size(-20, 0); 
+
+                
+
+                if ((i + 1) == Math.Pow(2, n + 1)) 
+                {
+                    n++;
+                    tam = tam / 2; 
+                    xo = tam;
+                    yo += 60;
+                }
+                else
+                {
+                    xo += (2 * tam);
+
+                }
+
+                i++; 
+                estado = true; 
+                ec = false;
+                tabPage1.Refresh(); 
+                txtnumeros.Clear(); 
+                txtnumeros.Focus();
+            }
+            catch
+            {
+                MessageBox.Show("Valor no valido"); //error
+            }
+        }
+
+        //EVENTO LIMPIAR
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //LIMPIAMOS LA PANTALLA 
+            n = 0;
+            i = 1;
+            tam = tabPage1.Width / 2;
+            xo = tam;
+            yo = 20;
+            tabPage1.Controls.Clear();
+            tabPage1.Refresh();
+            Array.Resize<int>(ref Arreglo_numeros, 1);
+            Array.Resize<Button>(ref Arreglo, 1);
+
+        }
+
+        //EVENTO ORDENAR
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = 0;
+
+                int contador = 0;
+                float promedioso = 0;
+                tam = tabPage1.Width / 2;
+                xo = tam;
+                yo = 20;
+                tabPage1.Controls.Clear();
+                tabPage1.Refresh();
+
+                Array.Resize<Button>(ref Arreglo, 1);
+                for (int k = 0; k < Arreglo_numeros.Length; k++)
+                {
+                    promedioso += Arreglo_numeros[k];
+                }
+  
+                promedioso = promedioso / (Arreglo_numeros.Length-1);
+
+                for (int k = 0; k < Arreglo_numeros.Length - 1; k++)
+                {
+                    if (promedioso >= Arreglo_numeros[k])
+                    {
+                        contador++;
+                    }
+                }
+
+                for (int j = 0; j < Arreglo_numeros.Length; j++)
+                {
+                    for (int k = 0; k < Arreglo_numeros.Length - 1; k++)
+                    {
+                        if (contador >= Arreglo_numeros.Length - 1 - contador)
+                        {
+                            if (Arreglo_numeros[k] > Arreglo_numeros[k + 1])
+                            {
+                                int aux = Arreglo_numeros[k];
+                                Arreglo_numeros[k] = Arreglo_numeros[k + 1];
+                                Arreglo_numeros[k + 1] = aux;
+                            }
+                        }
+                        else
+                        {
+                            if (Arreglo_numeros[k] != 0)
+                            {
+                                if (Arreglo_numeros[k] < Arreglo_numeros[k + 1])
+                                {
+                                    int aux = Arreglo_numeros[k];
+                                    Arreglo_numeros[k] = Arreglo_numeros[k + 1];
+                                    Arreglo_numeros[k + 1] = aux;
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+
+                for (int j = 0; j < Arreglo_numeros.Length; j++)
+                {
+                    Array.Resize<Button>(ref Arreglo, j + 1);
+                    Arreglo[j] = new Button();
+
+                    Arreglo[j].Text = Arreglo_numeros[j].ToString();
+                    Arreglo[j].Height = 40;
+                    Arreglo[j].Width = 40;
+                    Arreglo[j].BackColor = Color.DarkViolet;
+                    Arreglo[j].ForeColor = Color.White;
+                    Arreglo[j].Font = new Font("Century Gothic", 12);
+                    if (j == 1) { Arreglo[j].Location = new Point(xo / 3, yo) + new Size(-20, 0); }
+                    else
+                    {
+                        Arreglo[j].Location = new Point(xo, yo) + new Size(-20, 0);
+                    }
+
+
+
+                    if ((j + 1) == Math.Pow(2, n + 1))
+                    {
+                        n++;
+                        tam = tam / 2;
+                        xo = tam;
+                        yo += 60;
+                    }
+                    else
+                    {
+                        xo += (2 * tam);
+
+                    }
+
+
+                    estado = true;
+                    ec = false;
+                    tabPage1.Refresh();
+                    txtnumeros.Clear();
+                    txtnumeros.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Lamentamos informarle que en este preciso espacio tiempo, todavia no hay ningun dato ingresado sobre el cual poder decidir un orden, le ruego de favor ingrese un valor para poder llevar acabo dicha accion, sin nada mas que agregar y esperando pueda seguir llevando a cabo la ejecucion del programa sin ningun tipo de problema, nos despedimos.\n ATT. Johan y Andres","FATAL_ERROR x0000058", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+        }
+
+        public void intercambio(ref Button[] botones, int a, int b)
+        {
+            string tem = botones[a].Text;
+            Point pa = botones[a].Location;
+            Point pb = botones[b].Location;
+
+            int diferenciaX = Math.Abs(pa.X - pb.X);
+            int diferenciaY = Math.Abs(pa.Y - pb.Y);
+
+            int x = 10;
+            int y = 10;
+            int t = 70;
+
+            while (y != diferenciaY + 10)
+            {
+                Thread.Sleep(t);
+                botones[a].Location += new Size(0, -10);
+                botones[b].Location += new Size(0, +10);
+                y += 10;
+            }
+
+            while (x != diferenciaX - diferenciaX % 10 + 10)
+            {
+                if (pa.X < pb.X)
+                {
+                    Thread.Sleep(t);
+                    botones[a].Location += new Size(+10, 0);
+                    botones[b].Location += new Size(-10, 0);
+                    x += 10;
+                }
+
+                else
+                {
+                    Thread.Sleep(t);
+                    botones[a].Location += new Size(-10, 0);
+                    botones[b].Location += new Size(+10, 0);
+                    x += 10;
+                }
+            }
+            botones[a].Text = botones[b].Text;
+            botones[b].Text = tem;
+            botones[b].Location = pb;
+            botones[a].Location = pa;
+            estado = true;
+            tabControl1.Refresh();
+
+        }
+
+       
+
+        public void Dibujar_Arreglo(ref Button[] botones, ref TabPage tb)
+        {
+            for (int j = 1; j < botones.Length; j++)
+            {
+                tb.Controls.Add(this.Arreglo[j]);
+            }
+        }
+
+        public void Dibujar_ramas(ref Button[] botones, ref TabPage tb, PaintEventArgs e)
+        {
+            Pen lapiz = new Pen(Color.Gray, 1.5f);
+            Graphics g;
+            g = e.Graphics;
+
+            for (int j = 1; j < Arreglo.Length; j++)
+            {
+                if (Arreglo[(2 * j)] != null)
+                {
+                    g.DrawLine(lapiz, Arreglo[j].Location.X, Arreglo[j].Location.Y + 20, Arreglo[(2 * j)].Location.X + 20, Arreglo[(2 * j)].Location.Y);
+                }
+
+                if (Arreglo[(2 * j)] != null)
+                {
+                    g.DrawLine(lapiz, Arreglo[j].Location.X + 40, Arreglo[j].Location.Y + 20, Arreglo[(2 * j)].Location.X + 20, Arreglo[(2 * j) + 1].Location.Y);
+                }
+            }
+        }
+
+        public void HPN()
+        {
+            int temp;
+            int x = Arreglo_numeros.Length;
+
+            for (int i = x - 1; i >= 1; i--)
+            {
+                intercambio(ref Arreglo, i, 1);
+                temp = Arreglo_numeros[i];
+                Arreglo_numeros[i] = Arreglo_numeros[1];
+                Arreglo_numeros[1] = temp;
+                x--;
+            }
+
+        }
+
+        public void Heap_Num()
+        {
+            ec = true;
+            int x = Arreglo.Length;
+            for (int i = (x) / 2; i > 0; i--) ;
+
+
+
+
+        }
+        public void Heap_MinNum()
+        {
+            ec = true;
+
+            int x = Arreglo.Length;
+
+            for (int i = (x) / 2; i > 0; i--)
+                Min_Num(Arreglo_numeros, x, i, ref Arreglo);
+        }
+
+        public void Max_Num(int[] a, int x, int indice, ref Button[] botones)
+        {
+            int izquierdo = (indice) * 2;
+            int derecho = (indice) * 2 + 1;
+            int mayor = 0;
+            if (izquierdo < x && a[izquierdo] > a[indice])
+            {
+                mayor = izquierdo;
+            }
+            else
+            {
+                mayor = indice;
+            }
+            if (derecho < x && a[derecho] > a[mayor])
+            {
+                mayor = derecho;
+            }
+
+            if (mayor != indice)
+            {
+                int tem = a[indice];
+                a[indice] = a[mayor];
+                a[mayor] = tem;
+
+                intercambio(ref Arreglo, mayor, indice);
+                Max_Num(a, x, mayor, ref botones);
+
+            }
+
+
+        }
+
+        public void Min_Num(int[] a, int x, int indice, ref Button[] botones)
+        {
+            
+        }
+        private void tabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            if (estado)
+            {
+                try
+                {
+                    Dibujar_Arreglo(ref Arreglo, ref tabPage1);
+                    Dibujar_ramas(ref Arreglo, ref tabPage1, e);
+                }
+                catch
+                {
+
+                }
+                estado = false;
+            }
+
+        }
+
+        int[] Arreglo_numeros; //Arreglo de numeros ingresado
+        Button[] Arreglo; //arreglo de botones  para simular valores ingresador 
+
+        //Inicializacion del formulario 
+        public Form1()
+        {
+            InitializeComponent();
+            tam = tabPage1.Width / 2;
+            xo = tam;  // el valor inicial de x sera la mitad del ancho del tabpage 
+            yo = 20; //el valor incial en y y sera en 20 
+            txtnumeros.Focus(); //Cursor en textbox
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (i == 1)
+                MessageBox.Show("No hay elementos que ordenar");
+            else
+            {
+                btnagregar.Enabled = false;
+                btnlimpiar.Enabled = false;
+                btnordenar.Enabled = false;
+                this.Cursor = Cursors.WaitCursor;
+
+                if (!ec)
+                {
+                    Heap_MinNum();
+                }
+                else
+                {
+                    HPN();
+                }
+
+                btnagregar.Enabled = true;
+                btnlimpiar.Enabled = true;
+                
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+
+    }
+
+}
